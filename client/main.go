d7fa58ddf3b8330527e61b84ef36acbb62a08147
ansh0014 <anshul.jagota@yahoo.com>
2026-03-24 00:43:19 +0530

    make full project

--- client/main.go
+++ client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
 	pb "Distributed-ORDER-system/proto"
@@ -11,16 +12,52 @@ import (
 )
 
 func main() {
-	conn, _ := grpc.Dial("localhost:50053", grpc.WithInsecure())
+
+	mode := "success" 
+
+	if len(os.Args) > 1 {
+		mode = os.Args[1]
+	}
+
+	log.Println("Mode:", mode)
+
+	conn, err := grpc.Dial("localhost:50053", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("Failed to connect:", err)
+	}
+	defer conn.Close()
+
 	client := pb.NewOrderServiceClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	var timeout time.Duration
+
+	switch mode {
+	case "timeout":
+		timeout = 1 * time.Second 
+	default:
+		timeout = 5 * time.Second 
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
-	res, _ := client.CreateOrder(ctx, &pb.OrderRequest{
+
+	if mode == "timeout" {
+		log.Println("Simulating delay...")
+		time.Sleep(3 * time.Second)
+	}
+
+	log.Println("Sending Order Request...")
+
+	res, err := client.CreateOrder(ctx, &pb.OrderRequest{
 		UserId: 1,
 		Amount: 100,
 	})
+
+	if err != nil {
+		log.Println("Request FAILED:", err)
+		return
+	}
 
 	log.Println("Final Result:", res.Status)
 }
